53db11bcd90f3c436b57436d99a00eacf0240fd8
Tanmaysonar-456 <tanmaysonar613@gmail.com>
2026-09-06 12:26:18 +0530

    Add public site configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+window.CLOSEOUTDESK_CONFIG = {
+  brand: "CloseoutDesk",
+  supportEmail: "support@example.com",
+  publicUrl: "https://tanmaysonar-456.github.io/closeoutdesk/",
+  mode: "local",
+  supabaseUrl: "",
+  supabaseAnonKey: ""
+};
